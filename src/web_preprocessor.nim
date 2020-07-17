7dba15f5b0c49a7ff999966105b8b9ae5c5b60b2
bung87 <crc32@qq.com>
2020-07-17 20:25:47 +0800

    o

--- src/web_preprocessor.nim
+++ src/web_preprocessor.nim
@@ -11,7 +11,7 @@ export pkgutils
 
 const pkgDeps = {".scss": @["https://github.com/zacharycarter/nim-sass"]}.toTable
 
-const dep2pro = {"nim_sass":"sass"}.toTable
+const dep2pro = {"nim_sass": "sass"}.toTable
 
 proc getDeps*(dir: string = getCurrentDir()): seq[string] =
   ## get all dependencies throght files extensions.
@@ -20,7 +20,7 @@ proc getDeps*(dir: string = getCurrentDir()): seq[string] =
     if ext in pkgDeps:
       result.add pkgDeps[ext]
 
-proc installDeps*(dir: string = getCurrentDir(),deps = getDeps(dir)) =
+proc installDeps*(dir: string = getCurrentDir(), deps = getDeps(dir)) =
   install(deps)
 
 proc isInstalled*(pkgUriOrName: string): bool =
@@ -34,13 +34,39 @@ proc isInstalled*(pkgUriOrName: string, pkgs: seq[string]): bool =
 
 
 when isMainModule:
+  import json,md5
   let deps = getDeps()
-  let notInstalled = deps.filterIt( not isInstalled(it) )
+  let notInstalled = deps.filterIt(not isInstalled(it))
   if notInstalled.len > 0:
     install(notInstalled)
-  echo notInstalled
-  let namedDeps = deps.mapIt( pathToValidPackageName(it) )
-  let names = namedDeps.mapIt( dep2pro[it] )
+  debugEcho "not installed:" & $notInstalled
+  let namedDeps = deps.mapIt(pathToValidPackageName(it))
+  let names = namedDeps.mapIt(dep2pro[it])
   for name in names:
     if not processorExists(name):
-      discard compileProcessor( name )
+      discard compileProcessor(name)
+  let srcDir = getCurrentDir() / "tests" / "static"
+  var dir,name,ext:string
+  var manifest:Table[string,string]
+  if not fileExists("manifest.json"):
+    var f = open("manifest.json",fmWrite)
+    for path in walkDirRec(srcDir):
+      (dir, name, ext) = splitFile(path)
+      manifest[ relativePath(path,getCurrentDir()) ] = getMD5(readFile( path ))
+    let output = pretty(%*manifest)
+    f.write(output)
+    f.close()
+    echo output
+  else:
+    manifest = parseFile("manifest.json").to(Table[string,string])
+    var fileChanged = newSeq[string]()
+    var rel:string
+    for path in walkDirRec(srcDir):
+      (dir, name, ext) = splitFile(path)
+      rel = relativePath(path,getCurrentDir())
+      if manifest[ rel ]  != getMD5(readFile( path )):
+        fileChanged.add rel
+        
+    debugEcho "manifest:" & $manifest
+    debugEcho "fileChanged:" & $fileChanged
+    
--- tests/test2.nim
+++ tests/test2.nim
@@ -4,9 +4,9 @@ import os
 import macros
 
 test "compile project file":
-  let bin = compileProjectFile(  "processors" / "sass.nim")
+  let bin = compileProjectFile("processors" / "sass.nim")
   when defined(windows):
-    check bin == absolutePath getProjectPath() / "src" / "web_preprocessor" /  "processors" / "sass.exe"
+    check bin == absolutePath getProjectPath() / "src" / "web_preprocessor" / "processors" / "sass.exe"
   else:
     check bin == absolutePath getProjectPath() / "src" / "web_preprocessor" / "processors" / "sass"
   removeFile(bin)
